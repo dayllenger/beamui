b78b243e3257009af9d0c795dcf17c5729142c0d
dayllenger <dayllenger@protonmail.com>
2018-10-18 23:39:13 +0300

    update basic example

--- examples/basic/src/app.d
+++ examples/basic/src/app.d
@@ -14,40 +14,59 @@ module app;
 
 import beamui;
 
+// mandatory, it does some initialization and then runs UIAppMain
 mixin APP_ENTRY_POINT;
 
 /// Entry point for application
 extern (C) int UIAppMain(string[] args)
 {
-    string[] resourceDirs = [
-        appendPath(exePath, "../../../res/"),
-    ];
-    // will use only existing directories
-    platform.resourceDirs = resourceDirs;
-
+    // load better theme
     platform.uiTheme = "light";
 
-    auto window = platform.createWindow("Basic example", null);
+    // create a window with 1x1 size and expand it to the size of content
+    Window window = platform.createWindow("Basic example", null, WindowFlag.expanded, 1, 1);
 
-    window.mainWidget = parseML(q{
-        Column {
-            padding: 10pt
-            Label { text: "Text" }
-            Spacer {}
-            Row {
-                Spacer {}
-                Button { id: btnOk; text: "OK" }
-                Button { id: btnCancel; text: "Cancel" }
-            }
-        }
-    });
+    // create some widgets to show
+    // Column arranges items vertically
+    Column pane = new Column;
+    pane.minWidth = 200;
+    pane.padding = Insets(15);
+        Label header = new Label("Header");
+        header.fontSize = 18;
+        EditLine ed1 = new EditLine("Hello");
+        EditLine ed2 = new EditLine("world");
+        CheckBox check = new CheckBox("Check me");
+    pane.add(header);
+    pane.add(ed1);
+    pane.add(ed2);
+    pane.add(check);
+        // Row organizes items horizontally
+        Row line = new Row;
+            Button ok = new Button("OK");
+            Button exit = new Button("Exit");
+        // let the buttons fill horizontal space
+        line.add(ok).fillWidth(true);
+        line.add(exit).fillWidth(true);
+    pane.add(line);
 
-    window.mainWidget.childByID!Button("btnCancel").clicked = delegate(Widget w) {
+    // disable OK button
+    ok.enabled = false;
+    // and enable it when the check box has been pressed
+    check.checkChanged = delegate(Widget src, bool checked) {
+        ok.enabled = checked;
+    };
+    // show message box on OK button click
+    ok.clicked = delegate(Widget src) {
+        window.showMessageBox("Message box"d, format("%s, %s!"d, ed1.text, ed2.text));
+    };
+    // close the window by clicking Exit
+    exit.clicked = delegate(Widget src) {
         window.close();
-        return true;
     };
 
+    // set main widget for the window and show it
+    window.mainWidget = pane;
     window.show();
-
+    // run event loop
     return platform.enterMessageLoop();
 }
