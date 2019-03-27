6317b09704474b37a871811db7b03ee643da43e7
dayllenger <dayllenger@protonmail.com>
2019-03-27 13:38:16 +0300

    add FBO status checking, improve GL errors output

--- src/beamui/graphics/gl/errors.d
+++ src/beamui/graphics/gl/errors.d
@@ -13,6 +13,7 @@ static if (USE_OPENGL):
 import std.conv : to;
 import beamui.core.logger;
 import beamui.graphics.gl.api : GLenum, glGetError, GL_NO_ERROR;
+import beamui.graphics.gl.api : glCheckFramebufferStatus, GL_FRAMEBUFFER, GL_FRAMEBUFFER_COMPLETE;
 
 /// Convenient wrapper around glGetError(). Usage: checkgl!glFunction(funcParams);
 template checkgl(alias func)
@@ -30,10 +31,23 @@ template checkgl(alias func)
 /// Check for GL error. If an error occured, reports about it in the logger and returns true
 bool checkError(string context = "", string functionName = __FUNCTION__, int line = __LINE__)
 {
+    static GLenum lastError;
+    static uint count;
+
     GLenum err = glGetError();
     if (err != GL_NO_ERROR)
     {
-        Log.e("OpenGL error ", glErrorToString(err), " at ", functionName, ":", line, " -- ", context);
+        if (err != lastError)
+        {
+            Log.fe("OpenGL error: %s (%s)\nat %s, line %s", glErrorToString(err), context, functionName, line);
+            lastError = err;
+            count = 1;
+        }
+        else
+        {
+            count++;
+            Log.fe("the same GL error %s time", count);
+        }
         return true;
     }
     return false;
@@ -64,4 +78,30 @@ string glErrorToString(GLenum err)
     default:
         return "Unknown GL error: " ~ to!string(err);
     }
+}
+
+/// Check GL framebuffer status and log if it's not ready for use. Returns true if ready
+bool checkFramebuffer(GLenum target = GL_FRAMEBUFFER)
+{
+    const status = glCheckFramebufferStatus(target);
+
+    string err;
+    switch (status)
+    {
+        case GL_FRAMEBUFFER_COMPLETE: return true;
+        case 0x8219: err = "GL_FRAMEBUFFER_UNDEFINED"; break;
+        case 0x8CD6: err = "GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT"; break;
+        case 0x8CD7: err = "GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT"; break;
+        case 0x8CD9: err = "GL_FRAMEBUFFER_INCOMPLETE_DIMENSIONS_EXT"; break;
+        case 0x8CDA: err = "GL_FRAMEBUFFER_INCOMPLETE_FORMATS_EXT"; break;
+        case 0x8CDB: err = "GL_FRAMEBUFFER_INCOMPLETE_DRAW_BUFFER"; break;
+        case 0x8CDC: err = "GL_FRAMEBUFFER_INCOMPLETE_READ_BUFFER"; break;
+        case 0x8CDD: err = "GL_FRAMEBUFFER_UNSUPPORTED"; break;
+        case 0x8D56: err = "GL_FRAMEBUFFER_INCOMPLETE_MULTISAMPLE"; break;
+        case 0x8DA8: err = "GL_FRAMEBUFFER_INCOMPLETE_LAYER_TARGETS"; break;
+        default:     err = to!string(status); break;
+    }
+
+    Log.e("FBO error: ", err);
+    return false;
 }
