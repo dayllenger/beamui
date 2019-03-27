2ba589165c34e7cb7c4f2f80578faeeba97dd194
dayllenger <dayllenger@protonmail.com>
2019-03-27 14:35:21 +0300

    make a workaround for ddox bug with repeating declarations

--- docs.sh
+++ docs.sh
@@ -3,6 +3,16 @@
 dub fetch ddox
 cp ./ddox/ddox.inc.composite.dt ~/.dub/packages/ddox-*/ddox/views/ddox.inc.composite.dt
 cp ./ddox/ddox.module.dt ~/.dub/packages/ddox-*/ddox/views/ddox.module.dt
+
+parser=~/.dub/packages/ddox-*/ddox/source/ddox/parsers/jsonparser.d
+size=$(wc -c < $parser)
+origSize=25965
+
+if [ $size -eq $origSize ]
+then
+    patch $parser ./ddox/jsonparser.diff
+fi
+
 dub build -b ddox
 cp ./ddox/ddox.css ./docs/styles/ddox.css
 cp ./ddox/prettify.css ./docs/prettify/prettify.css
