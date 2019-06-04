bed521061ad1fb3b1da6a50348c3daf2beba1943
dayllenger <dayllenger@protonmail.com>
2019-06-05 00:47:39 +0300

    add utils for bracket matching

--- src/beamui/core/parseutils.d
+++ src/beamui/core/parseutils.d
@@ -33,11 +33,120 @@ bool isPunct(dchar ch)
 {
     return ch == '.' || ch == ',' || ch == ';' || ch == '?' || ch == '!';
 }
-/// Returns true whether char is some bracket
+
+//===============================================================
+// Brackets
+
+/// Returns true if the char is a `()[]{}` bracket
 bool isBracket(dchar ch)
 {
     return ch == '(' || ch == ')' || ch == '[' || ch == ']' || ch == '{' || ch == '}';
 }
+
+/// Returns the paired bracket for the , if it's one of `()[]{}`, otherwise returns 0
+dchar getPairedBracket(dchar ch)
+{
+    switch (ch)
+    {
+        case '(': return ')';
+        case ')': return '(';
+        case '{': return '}';
+        case '}': return '{';
+        case '[': return ']';
+        case ']': return '[';
+        default: return 0; // not a bracket
+    }
+}
+
+/// Returns true if the char is an opening bracket
+bool isOpeningBracket(dchar ch)
+{
+    return ch == '(' || ch == '{' || ch == '[';
+}
+
+/// Returns true if the char is a closing bracket
+bool isClosingBracket(dchar ch)
+{
+    return ch == ')' || ch == '}' || ch == ']';
+}
+
+/// Basic stack to count matching brackets
+struct BracketStack
+{
+    import beamui.core.collections : Buf;
+
+    enum Match
+    {
+        continue_,
+        found,
+        error
+    }
+
+    private Buf!dchar buf;
+    private bool reverse;
+
+    void initialize(bool reverse)
+    {
+        this.reverse = reverse;
+        buf.clear();
+    }
+
+    BracketStack.Match process(dchar ch)
+    {
+        if (reverse)
+        {
+            if (isClosingBracket(ch))
+            {
+                push(ch);
+                return Match.continue_;
+            }
+            else
+            {
+                if (pop() != getPairedBracket(ch))
+                    return Match.error;
+                if (buf.length == 0)
+                    return Match.found;
+                return Match.continue_;
+            }
+        }
+        else
+        {
+            if (isOpeningBracket(ch))
+            {
+                push(ch);
+                return Match.continue_;
+            }
+            else
+            {
+                if (pop() != getPairedBracket(ch))
+                    return Match.error;
+                if (buf.length == 0)
+                    return Match.found;
+                return Match.continue_;
+            }
+        }
+    }
+
+    private void push(dchar ch)
+    {
+        buf.put(ch);
+    }
+
+    private dchar pop()
+    {
+        if (buf.length > 0)
+        {
+            const ret = buf[$ - 1];
+            buf.shrink(1);
+            return ret;
+        }
+        else
+            return 0;
+    }
+}
+
+//===============================================================
+// Number parsing
 
 /// Decodes hex digit (0..9, a..f, A..F), returns uint.max if invalid
 uint parseHexDigit(T)(T ch)
