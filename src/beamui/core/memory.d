8b99a6912267d2b55a4fa487caf530c026322cdc
dayllenger <dayllenger@protonmail.com>
2020-02-21 19:09:41 +0300

    add an arena allocator for future use

---
+++ src/beamui/core/memory.d
@@ -0,0 +1,65 @@
+/**
+
+Copyright: dayllenger 2020
+License:   Boost License 1.0
+Authors:   dayllenger
+*/
+module beamui.core.memory;
+
+import std.conv : emplace;
+import beamui.core.ownership : isReferenceType;
+
+struct Arena
+{
+    private void[] buf;
+    private size_t len;
+
+    /// Allocate and construct a class instance
+    T make(T, Args...)(Args args) if (is(T == class))
+    {
+        // align to 16 bytes
+        enum alignedSize = (__traits(classInstanceSize, T) | 0b1111) + 1;
+
+        const site = len;
+        len += alignedSize;
+        if (buf.length < len)
+            buf.length = len * 2;
+
+        return emplace!T(cast(T)&buf[site], args);
+    }
+
+    /// Allocate and initialize a struct instance
+    T* make(T)() if (is(T == struct))
+    {
+        const site = len;
+        len += T.sizeof;
+        if (buf.length < len)
+            buf.length = len * 2;
+
+        T* obj = cast(T*)&buf[site];
+        *obj = T.init;
+        return obj;
+    }
+
+    /// Allocate an array of size `count`, filled with nulls
+    T[] allocArray(T)(size_t count) if (isReferenceType!T)
+    {
+        if (count == 0)
+            return null;
+
+        const site = len;
+        len += count * T.sizeof;
+        if (buf.length < len)
+            buf.length = len * 2;
+
+        T[] array = (cast(T*)&buf[site])[0 .. count];
+        array[] = null;
+        return array;
+    }
+
+    /// Clear the arena, retaining allocated memory
+    void clear()
+    {
+        len = 0;
+    }
+}
