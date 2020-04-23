8b44178547a46694447d94bafbc2aac25568ccad
dayllenger <dayllenger@protonmail.com>
2020-04-23 20:13:17 +0300

    rewrite window frame widget

--- src/beamui/widgets/winframe.d
+++ src/beamui/widgets/winframe.d
@@ -1,81 +1,54 @@
 /**
 Window frame widget.
 
-Copyright: Vadim Lopatin 2015
+Copyright: Vadim Lopatin 2015, dayllenger 2020
 License:   Boost License 1.0
 Authors:   Vadim Lopatin
 */
 module beamui.widgets.winframe;
-/+
-import beamui.widgets.controls;
+
+import beamui.widgets.controls : Button;
 import beamui.widgets.text;
 import beamui.widgets.widget;
 
 /// Window frame with caption widget
 class WindowFrame : Panel
 {
-    @property Widget bodyWidget() { return _bodyWidget; }
-    /// ditto
-    @property void bodyWidget(Widget widget)
+    dstring title;
+    /// If assigned, the close button will be visible
+    void delegate() onClose;
+
+    protected Widget _content;
+
+    final WindowFrame wrap(Widget content)
     {
-        _bodyLayout.replaceChild(_bodyWidget, widget);
-        destroy(_bodyWidget);
-        _bodyWidget = widget;
+        _content = content;
+        return this;
     }
 
-    @property Label title() { return _title; }
+    protected alias wrap = typeof(super).wrap;
 
-    Signal!(void delegate()) onCloseButtonClick;
-
-    private
+    override protected void build()
     {
-        Widget _bodyWidget;
-        Panel _titleLayout;
-        Label _title;
-        Button _closeButton;
-        bool _showCloseButton;
-        Panel _bodyLayout;
-    }
-
-    this(bool showCloseButton = true)
-    {
-        _showCloseButton = showCloseButton;
-        initialize();
-    }
-
-    protected void initialize()
-    {
-        _titleLayout = new Panel(null, "caption");
-            _title = new Label;
-            _closeButton = new Button(null, "close");
-        _bodyLayout = new Panel(null, "body");
-            _bodyWidget = createBodyWidget();
-
-        with (_titleLayout) {
-            isolateThisStyle();
-            add(_title, _closeButton);
-        }
-        with (_title) {
-            isolateThisStyle();
-            setAttribute("label");
-        }
-        with (_closeButton) {
-            setAttribute("flat");
-        }
-        with (_bodyLayout) {
-            isolateThisStyle();
-            add(_bodyWidget);
-        }
-        add(_titleLayout, _bodyLayout);
-
-        _closeButton.onClick ~= &onCloseButtonClick.emit;
-        if (!_showCloseButton)
-            _closeButton.visibility = Visibility.gone;
-    }
-
-    protected Widget createBodyWidget()
-    {
-        return new Widget;
+        wrap(
+            render((Panel p) {
+                p.attributes["caption"];
+                p.isolateThisStyle = true;
+            }).wrap(
+                render((Label lb) {
+                    lb.text = title;
+                    lb.isolateThisStyle = true;
+                }),
+                onClose ? render((Button b) {
+                    b.attributes["flat"];
+                    b.iconID = "close";
+                    b.onClick = onClose;
+                }) : null,
+            ),
+            render((Panel p) {
+                p.attributes["body"];
+                p.isolateThisStyle = true;
+            }).wrap(_content),
+        );
     }
 }
-+/
