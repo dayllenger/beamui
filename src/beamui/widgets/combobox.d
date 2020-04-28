ee298af892e186d1e5d0a9c8a9a1836f4e8184a0
dayllenger <dayllenger@protonmail.com>
2020-04-28 03:23:34 +0300

    port comboboxes (mostly)

--- src/beamui/widgets/combobox.d
+++ src/beamui/widgets/combobox.d
@@ -15,422 +15,290 @@ cbox.selectedItemIndex = 0;
 writeln(cbox.text);
 ---
 
-Copyright: Vadim Lopatin 2014-2017, dayllenger 2019
+Copyright: Vadim Lopatin 2014-2017, dayllenger 2019-2020
 License:   Boost License 1.0
-Authors:   Vadim Lopatin
+Authors:   Vadim Lopatin, dayllenger
 */
 module beamui.widgets.combobox;
-/+
-import beamui.widgets.controls;
-import beamui.widgets.editors;
-import beamui.widgets.lists;
+
+import beamui.widgets.controls : Button, ImageWidget;
+import beamui.widgets.editors : EditLine;
+import beamui.widgets.lists : ElemListView, ListView;
 import beamui.widgets.popup;
-import beamui.widgets.text;
+import beamui.widgets.text : Label;
 import beamui.widgets.widget;
 
-/// Abstract ComboBox
-abstract class ComboBoxBase : Panel
+private class ComboListView : ListView
 {
-    @property
+    override protected Element createElement()
     {
-        /// Selected item index
-        int selectedItemIndex() const { return _selectedItemIndex; }
-        /// ditto
-        void selectedItemIndex(int index)
-        {
-            if (_selectedItemIndex == index)
-                return;
-            if (_selectedItemIndex != -1 && _adapter.itemCount > _selectedItemIndex)
-            {
-                _adapter.resetItemState(_selectedItemIndex, State.selected | State.focused | State.hovered);
-            }
-            _selectedItemIndex = index;
-            if (_popupList)
-                _popupList.selectItem(index);
-            onSelect(index);
-        }
-    }
-
-    /// Triggers on item selection and passes integer index of the item
-    Signal!(void delegate(int)) onSelect;
-
-    private
-    {
-        Widget _body;
-        Button _button;
-        ListAdapter _adapter;
-        bool _ownAdapter;
-        int _selectedItemIndex;
-    }
-
-    this(ListAdapter adapter, bool ownAdapter = true)
-    {
-        _adapter = adapter;
-        _ownAdapter = ownAdapter;
-        isolateStyle();
-        allowsHover = true;
-        initialize();
-    }
-
-    ~this()
-    {
-        if (_ownAdapter)
-            eliminate(_adapter);
-    }
-
-    void setAdapter(ListAdapter adapter, bool ownAdapter = true)
-    {
-        if (_adapter)
-        {
-            if (_ownAdapter)
-                destroy(_adapter);
-            removeAllChildren();
-        }
-        _adapter = adapter;
-        _ownAdapter = ownAdapter;
-        initialize();
-    }
-
-    protected void initialize()
-    {
-        _body = createSelectedItemWidget();
-        _body.setAttribute("body");
-        _body.allowsFocus = false;
-        _body.onClick ~= &handleClick;
-
-        _button = createButton();
-        _button.setAttribute("button");
-        _button.allowsFocus = false;
-        _button.onClick ~= &handleClick;
-
-        add(_body, _button);
-        allowsFocus = true;
-    }
-
-    protected Widget createSelectedItemWidget();
-
-    protected Button createButton()
-    {
-        return new Button(null, "scrollbar_btn_down");
-    }
-
-    protected ListWidget createPopup()
-    {
-        auto list = new ListWidget;
-        list.adapter = _adapter;
-        list.selectedItemIndex = _selectedItemIndex;
-        list.sumItemSizes = true;
-        return list;
-    }
-
-    private bool _popupShown;
-    private ListWidget _popupList;
-
-    protected void showPopup()
-    {
-        if (!_adapter || !_adapter.itemCount)
-            return; // don't show empty popup
-
-        _popupShown = true;
-        _popupList = createPopup();
-        Popup popup = window.showPopup(_popupList);
-        popup.setAttribute("combobox");
-        popup.anchor = WeakRef!Widget(this);
-        popup.alignment = PopupAlign.below | PopupAlign.fitAnchorSize;
-        popup.onPopupClose ~= (bool b) {
-            _popupShown = false;
-            _popupList = null;
-            removeAttribute("opened");
-            handlePopupClose();
-        };
-        setAttribute("opened");
-        _popupList.onItemClick ~= (int index) {
-            selectedItemIndex = index;
-            if (popup)
-                popup.close();
-        };
-        _popupList.onKeyEvent ~= (KeyEvent e) {
-            if (e.action == KeyAction.keyDown && e.key == Key.escape && e.noModifiers)
-            {
-                if (popup)
-                {
-                    popup.close();
-                    return true;
-                }
-            }
-            return false;
-        };
-        _popupList.setFocus();
-    }
-
-    protected void handlePopupClose()
-    {
-        setFocus();
-    }
-
-    override protected void handleClick()
-    {
-        if (!_popupShown)
-        {
-            showPopup();
-        }
-    }
-
-    override bool handleWheelEvent(WheelEvent event)
-    {
-        const delta = event.deltaY > 0 ? 1 : -1;
-        const oldIndex = selectedItemIndex;
-        const newIndex = clamp(oldIndex + delta, 0, _adapter.itemCount - 1);
-        selectedItemIndex = newIndex;
-        return oldIndex != newIndex;
+        auto el = new ElemListView;
+        el.sumItemSizes = true;
+        return el;
     }
 }
 
-/// ComboBox with list of strings
-class ComboBox : ComboBoxBase
+/// Abstract combobox widget
+abstract class ComboBoxBase : Panel
 {
-    @property
-    {
-        void items(dstring[] items)
-        {
-            _selectedItemIndex = -1;
-            setAdapter(new StringListAdapter(items));
-            if (items.length > 0)
-            {
-                if (selectedItemIndex == -1 || selectedItemIndex > items.length)
-                    selectedItemIndex = 0;
-            }
-            requestLayout();
-        }
-
-        void items(StringListValue[] items)
-        {
-            _selectedItemIndex = -1;
-            if (auto a = cast(StringListAdapter)_adapter)
-                a.items = items;
-            else
-                setAdapter(new StringListAdapter(items));
-            if (items.length > 0)
-            {
-                selectedItemIndex = 0;
-            }
-            requestLayout();
-        }
-
-        /// Get selected item as text
-        dstring selectedItem()
-        {
-            if (_selectedItemIndex < 0 || _selectedItemIndex >= _adapter.itemCount)
-                return null;
-            return adapter.item(_selectedItemIndex);
-        }
-
-        inout(StringListAdapter) adapter() inout
-        {
-            return cast(inout(StringListAdapter))_adapter;
-        }
-
-        override dstring text() const
-        {
-            return _body.text;
-        }
-        override void text(dstring txt)
-        {
-            const idx = adapter.find(txt);
-            if (idx >= 0)
-            {
-                selectedItemIndex = idx;
-            }
-            else
-            {
-                // not found
-                _selectedItemIndex = -1;
-                _body.text = txt;
-            }
-        }
-
-        override int selectedItemIndex() const
-        {
-            return super.selectedItemIndex;
-        }
-        override void selectedItemIndex(int index)
-        {
-            _body.text = adapter.item(index);
-            super.selectedItemIndex = index;
-        }
-    }
+    /// Triggers on item selection and passes integer index of the item
+    void delegate(int) onSelect;
 
     this()
     {
-        super(new StringListAdapter, true);
-    }
-
-    this(dstring[] items)
-    {
-        super(new StringListAdapter(items), true);
-    }
-
-    this(StringListValue[] items)
-    {
-        super(new StringListAdapter(items), true);
-    }
-
-    override void initialize()
-    {
-        super.initialize();
-        _body.allowsFocus = false;
-        _body.allowsClick = true;
         allowsFocus = true;
-        allowsClick = true;
+        allowsHover = true;
+        isolateStyle = true;
+        onWheelEvent = &handleWheelEvent;
     }
 
-    override protected Widget createSelectedItemWidget()
+    private bool handleWheelEvent(WheelEvent event)
     {
-        auto label = new Label;
-        label.allowsClick = true;
-        return label;
+        const delta = event.deltaY > 0 ? 1 : -1;
+        const oldIndex = getState().selectedItemIndex;
+        const newIndex = clamp(oldIndex + delta, 0, itemCount - 1);
+        select(newIndex);
+        return oldIndex != newIndex;
+    }
+
+protected:
+
+    static class State : IState
+    {
+        int selectedItemIndex;
+        bool opened;
+    }
+
+    State getState()
+    {
+        return useState(new State);
+    }
+
+    void open()
+    {
+        if (itemCount > 0) // don't show empty popup
+        {
+            setState(getState().opened, true);
+        }
+    }
+
+    void close()
+    {
+        setState(getState().opened, false);
+        // TODO: focus combobox back
+    }
+
+    void select(int index)
+    {
+        auto st = getState();
+        if (st.selectedItemIndex != index)
+        {
+            setState(st.selectedItemIndex, index);
+            if (onSelect)
+                onSelect(index);
+        }
+    }
+
+    override void build()
+    {
+        Widget body = buildBody();
+        Widget arrow = buildArrow();
+        body.attributes["body"];
+        arrow.attributes["arrow"];
+        wrap(body, arrow);
+
+        const st = getState();
+        if (st.opened)
+        {
+            attributes["opened"];
+
+            // TODO: focus list
+            ListView v = buildList();
+            v.itemCount = itemCount;
+            v.onItemClick = (i) {
+                select(i);
+                close();
+            };
+            v.onKeyEvent = (KeyEvent e) {
+                if (e.action == KeyAction.keyDown && e.key == Key.escape && e.noModifiers)
+                {
+                    close();
+                    return true;
+                }
+                return false;
+            };
+
+            Popup p = render!Popup;
+            p.attributes["combobox"];
+            p.anchor = this;
+            p.alignment = PopupAlign.below | PopupAlign.fitAnchorSize;
+            p.wrap(v);
+            window.showPopup(p);
+        }
+    }
+
+    abstract int itemCount() const;
+    abstract Widget buildBody() out(w; w);
+    abstract Widget buildArrow() out(w; w);
+    abstract ListView buildList() out(w; w);
+}
+
+/// Combobox with list of strings
+class ComboBox : ComboBoxBase
+{
+    const(dstring)[] items;
+
+protected:
+
+    override int itemCount() const
+    {
+        return cast(int)items.length;
+    }
+
+    override Widget buildBody()
+    {
+        const st = getState();
+        Label t = render!Label;
+        t.text = items[st.selectedItemIndex];
+        return t;
+    }
+
+    override Widget buildArrow()
+    {
+        ImageWidget img = render!ImageWidget;
+        img.imageID = "scrollbar_btn_down";
+        return img;
+    }
+
+    override ListView buildList()
+    {
+        ListView v = render!ComboListView;
+        v.itemBuilder = i => v.item(items[i]);
+        return v;
+    }
+
+    override Element createElement()
+    {
+        auto el = new ElemPanel;
+        el.allowsClick = true;
+        return el;
+    }
+
+    override void updateElement(Element el)
+    {
+        super.updateElement(el);
+        el.onClick.clear();
+        el.onClick ~= &open;
     }
 }
 
-/// ComboBox with list of strings
+/// Combobox with list of strings with icons
 class IconTextComboBox : ComboBoxBase
 {
-    @property
+    const(StringListValue)[] items;
+
+protected:
+
+    override int itemCount() const
     {
-        void items(StringListValue[] items)
-        {
-            _selectedItemIndex = -1;
-            if (auto a = cast(IconStringListAdapter)_adapter)
-                a.items = items;
-            else
-                setAdapter(new IconStringListAdapter(items));
-            if (items.length > 0)
-            {
-                selectedItemIndex = 0;
-            }
-            requestLayout();
-        }
-
-        /// Get selected item as text
-        dstring selectedItem() const
-        {
-            if (_selectedItemIndex < 0 || _selectedItemIndex >= _adapter.itemCount)
-                return null;
-            return adapter.item(_selectedItemIndex);
-        }
-
-        inout(IconStringListAdapter) adapter() inout
-        {
-            return cast(inout(IconStringListAdapter))_adapter;
-        }
-
-        override dstring text() const
-        {
-            return _body.text;
-        }
-        override void text(dstring txt)
-        {
-            const idx = adapter.find(txt);
-            if (idx >= 0)
-            {
-                selectedItemIndex = idx;
-            }
-            else
-            {
-                // not found
-                _selectedItemIndex = -1;
-                _body.text = txt;
-            }
-        }
-
-        override int selectedItemIndex() const
-        {
-            return super.selectedItemIndex;
-        }
-        override void selectedItemIndex(int index)
-        {
-            _body.text = adapter.item(index);
-            super.selectedItemIndex = index;
-        }
+        return cast(int)items.length;
     }
 
-    this(StringListValue[] items = null)
+    override Widget buildBody()
     {
-        super(new IconStringListAdapter(items), true);
+        const st = getState();
+        Label t = render!Label;
+        t.text = items[st.selectedItemIndex].label;
+        return t;
     }
 
-    override void initialize()
+    override Widget buildArrow()
     {
-        super.initialize();
-        _body.allowsFocus = false;
-        _body.allowsClick = true;
-        allowsFocus = true;
-        allowsClick = true;
+        ImageWidget img = render!ImageWidget;
+        img.imageID = "scrollbar_btn_down";
+        return img;
     }
 
-    override protected Widget createSelectedItemWidget()
+    override ListView buildList()
     {
-        auto label = new Label;
-        label.allowsClick = true;
-        return label;
+        ListView v = render!ComboListView;
+        v.itemBuilder = i => v.item(items[i]);
+        return v;
+    }
+
+    override Element createElement()
+    {
+        auto el = new ElemPanel;
+        el.allowsClick = true;
+        return el;
+    }
+
+    override void updateElement(Element el)
+    {
+        super.updateElement(el);
+        el.onClick.clear();
+        el.onClick ~= &open;
     }
 }
 
-/// Editable ComboBox with list of strings
-class ComboEdit : ComboBox
+/// Editable combobox with list of strings
+class ComboEdit : ComboBoxBase
 {
-    @property bool readOnly() const
-    {
-        return _edit.readOnly;
-    }
-    /// ditto
-    @property void readOnly(bool ro)
-    {
-        _edit.readOnly = ro;
-    }
+    const(dstring)[] items;
+    bool readOnly;
 
-    private EditLine _edit;
-
-    this(dstring[] items = null)
+    this()
     {
-        super(items);
-    }
-
-    override void initialize()
-    {
-        super.initialize();
-        _edit.allowsFocus = true;
-        allowsClick = false;
         allowsFocus = false;
+        onKeyEvent = &handleKeyEvent;
     }
 
-    override protected Widget createSelectedItemWidget()
+    private bool handleKeyEvent(KeyEvent event)
     {
-        _edit = new EditLine;
-        return _edit;
+        if (event.action == KeyAction.keyDown)
+        {
+            if (event.key == Key.down)
+            {
+                open();
+                return true;
+            }
+            if ((event.key == Key.space || event.key == Key.enter) && readOnly)
+            {
+                open();
+                return true;
+            }
+        }
+        return false;
     }
 
-    override bool handleKeyEvent(KeyEvent event)
+protected:
+
+    override int itemCount() const
     {
-        if (event.key == Key.down)
-        {
-            if (event.action == KeyAction.keyDown)
-            {
-                showPopup();
-            }
-            return true;
-        }
-        if ((event.key == Key.space || event.key == Key.enter) && readOnly)
-        {
-            if (event.action == KeyAction.keyDown)
-            {
-                showPopup();
-            }
-            return true;
-        }
-        if (_edit.handleKeyEvent(event))
-            return true;
-        return super.handleKeyEvent(event);
+        return cast(int)items.length;
+    }
+
+    override Widget buildBody()
+    {
+        const st = getState();
+        EditLine ed = render!EditLine;
+        ed.text = items[st.selectedItemIndex];
+        ed.readOnly = readOnly;
+        return ed;
+    }
+
+    override Widget buildArrow()
+    {
+        Button btn = render!Button;
+        btn.iconID = "scrollbar_btn_down";
+        btn.onClick = &open;
+        return btn;
+    }
+
+    override ListView buildList()
+    {
+        ListView v = render!ComboListView;
+        v.itemBuilder = i => v.item(items[i]);
+        return v;
     }
 }
-+/
