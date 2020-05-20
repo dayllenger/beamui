d8c2668871ade88d3d18bc33c51da7b6bd7a492e
dayllenger <dayllenger@protonmail.com>
2020-05-20 19:19:34 +0300

    add CSS hot reload tool

--- examples/css/src/app.d
+++ examples/css/src/app.d
@@ -1,6 +1,7 @@
 module app;
 
 import beamui;
+import beamui.tools.css_hot_reload;
 
 mixin RegisterPlatforms;
 
@@ -23,7 +24,7 @@ int main()
     setStyleSheet(currentTheme, styles);
 
     Window window1 = platform.createWindow("CSS sandbox - beamui");
-    Window window2 = platform.createWindow("CSS hot-reloader", window1, WindowOptions.expanded, 1, 1);
+    Window window2 = platform.createWindow("CSS hot reloader", window1, WindowOptions.expanded, 1, 1);
 
     window1.onClose = {
         window1 = null;
@@ -37,103 +38,12 @@ int main()
     };
 
     window1.show(() => render!Controls);
-    window2.show(() => render!CssHotReloadWidget);
+    window2.show({
+        return render((CssHotReloadWidget w) {
+            w.defaultStyleSheet = DEFAULT_STYLE;
+        });
+    });
     return platform.enterMessageLoop();
-}
-
-class CssHotReloadWidget : Panel
-{
-    protected static class State : IState
-    {
-        bool watching;
-        bool error;
-
-        dstring resourceID = DEFAULT_STYLE;
-        FileMonitor fmon;
-
-        void watch(Window win)
-        {
-            const filename = resourceList.getPathByID(toUTF8(resourceID));
-            fmon = FileMonitor(filename);
-            if (fmon.check() == FileMonitor.Status.missing)
-            {
-                setState(error, true);
-                return;
-            }
-            updateStyles();
-            win.setTimer(1000, {
-                if (!watching)
-                    return false;
-
-                const status = fmon.check();
-                if (status == FileMonitor.Status.modified)
-                {
-                    updateStyles();
-                }
-                else if (status == FileMonitor.Status.missing)
-                {
-                    setState(watching, false);
-                    setState(error, true);
-                }
-                return true;
-            });
-            setState(watching, true);
-            setState(error, false);
-        }
-
-        void updateStyles()
-        {
-            const filename = resourceList.getPathByID(toUTF8(resourceID));
-            const styles = cast(string)loadResourceBytes(filename);
-            platform.reloadTheme();
-            setStyleSheet(currentTheme, styles);
-        }
-    }
-
-    override protected void build()
-    {
-        State st = useState!State;
-        wrap(
-            render((Label tip) {
-                tip.text = "Style resource ID:";
-            }),
-            render((EditLine ed) {
-                ed.text = st.resourceID;
-                if (!st.watching)
-                    ed.onChange = (s) { st.resourceID = s; };
-                else
-                    ed.readOnly = true;
-            }),
-            render((CheckButton b) {
-                b.text = "Watch";
-                b.checked = st.watching;
-                b.onToggle = (v) {
-                    if (v)
-                        st.watch(window);
-                    else
-                        setState(st.watching, false);
-                };
-            }),
-            render((Button b) {
-                b.text = "Reload manually";
-                b.onClick = &st.updateStyles;
-            }),
-            render((Label tip) {
-                if (st.watching)
-                {
-                    tip.text = "Status: watching";
-                    tip.attributes["state"] = "watching";
-                }
-                else if (st.error)
-                {
-                    tip.text = "Status: no such file";
-                    tip.attributes["state"] = "error";
-                }
-                else
-                    tip.text = "Status: not watching";
-            }),
-        );
-    }
 }
 
 class Controls : Panel
---
+++ tools/src/beamui/tools/css_hot_reload.d
@@ -0,0 +1,111 @@
+/**
+
+Copyright: dayllenger 2020
+License:   Boost License 1.0
+Authors:   dayllenger
+*/
+module beamui.tools.css_hot_reload;
+
+import beamui;
+
+class CssHotReloadWidget : Panel
+{
+    dstring defaultStyleSheet;
+
+    protected static class State : IState
+    {
+        bool watching;
+        bool error;
+
+        dstring resourceID;
+        FileMonitor fmon;
+
+        this(dstring resourceID)
+        {
+            this.resourceID = resourceID;
+        }
+
+        void watch(Window win)
+        {
+            const filename = resourceList.getPathByID(toUTF8(resourceID));
+            fmon = FileMonitor(filename);
+            if (fmon.check() == FileMonitor.Status.missing)
+            {
+                setState(error, true);
+                return;
+            }
+            updateStyles();
+            win.setTimer(1000, {
+                if (!watching)
+                    return false;
+
+                const status = fmon.check();
+                if (status == FileMonitor.Status.modified)
+                {
+                    updateStyles();
+                }
+                else if (status == FileMonitor.Status.missing)
+                {
+                    setState(watching, false);
+                    setState(error, true);
+                }
+                return true;
+            });
+            setState(watching, true);
+            setState(error, false);
+        }
+
+        void updateStyles()
+        {
+            const filename = resourceList.getPathByID(toUTF8(resourceID));
+            const styles = cast(string)loadResourceBytes(filename);
+            platform.reloadTheme();
+            setStyleSheet(currentTheme, styles);
+        }
+    }
+
+    override protected void build()
+    {
+        State st = useState(new State(defaultStyleSheet));
+        wrap(
+            render((Label tip) {
+                tip.text = "Style resource ID:";
+            }),
+            render((EditLine ed) {
+                ed.text = st.resourceID;
+                if (!st.watching)
+                    ed.onChange = (s) { st.resourceID = s; };
+                else
+                    ed.readOnly = true;
+            }),
+            render((CheckButton b) {
+                b.text = "Watch";
+                b.checked = st.watching;
+                b.onToggle = (v) {
+                    if (v)
+                        st.watch(window);
+                    else
+                        setState(st.watching, false);
+                };
+            }),
+            render((Button b) {
+                b.text = "Reload manually";
+                b.onClick = &st.updateStyles;
+            }),
+            render((Label tip) {
+                if (st.watching)
+                {
+                    tip.text = "Status: watching";
+                    tip.attributes["state"] = "watching";
+                }
+                else if (st.error)
+                {
+                    tip.text = "Status: no such file";
+                    tip.attributes["state"] = "error";
+                }
+                else
+                    tip.text = "Status: not watching";
+            }),
+        );
+    }
+}
