b1b7c4f5eb7e1599e60bbff387638b2b4a7ff35d
dayllenger <dayllenger@protonmail.com>
2020-06-14 22:06:16 +0300

    port some widgets for settings dialog

--- src/beamui/dialogs/settingsdialog.d
+++ src/beamui/dialogs/settingsdialog.d
@@ -12,91 +12,65 @@ import std.path;
 import beamui.core.files;
 import beamui.core.i18n;
 import beamui.core.parseutils;
-import beamui.core.settings;
 import beamui.core.stdaction;
 import beamui.dialogs.dialog;
 import beamui.layout.linear : Resizer;
 import beamui.platforms.common.platform;
 import beamui.widgets.combobox;
-import beamui.widgets.controls;
-import beamui.widgets.editors;
 import beamui.widgets.lists;
 import beamui.widgets.menu;
-import beamui.widgets.text;
 import beamui.widgets.tree;
++/
+import beamui.core.settings;
+import beamui.dialogs.filedialog;
+import beamui.widgets.controls;
+import beamui.widgets.editors : EditLine;
+import beamui.widgets.text : Label;
 import beamui.widgets.widget;
 
 /// Item on settings page
-class SettingsItem
+class SettingsItem : WidgetWrapperOf!Widget
 {
     /// Setting path, e.g. "editor/tabSize"
-    @property string id() const { return _id; }
+    string path;
+    dstring title;
 
-    @property dstring label() const { return _label; }
+    protected Setting settings;
 
-    private
+    override protected Element createElement()
     {
-        string _id;
-        dstring _label;
-        SettingsPage _page;
-    }
-
-    this(string id, dstring label)
-    {
-        _id = id;
-        _label = label;
-    }
-
-    /// Create setting widget
-    Widget[] createWidgets(Setting settings)
-    {
-        auto res = new Label(_label);
-        res.id = _id;
-        return [res];
+        return new ElemPanel;
     }
 }
 
-/// Checkbox setting
+/// Checkbox setting (boolean value)
 class CheckboxItem : SettingsItem
 {
-    private bool _inverse;
+    bool defaultValue;
 
-    this(string id, dstring label, bool inverse = false)
+    override protected void build()
     {
-        super(id, label);
-        _inverse = inverse;
-    }
+        assert(settings);
+        Setting item = settings.settingByPath(path);
 
-    override Widget[] createWidgets(Setting settings)
-    {
-        auto cb = new CheckBox(_label);
-        cb.id = _id;
-        cb.style.minWidth = 60;
-        Setting setting = settings.settingByPath(_id);
-        cb.checked = setting.boolean = setting.boolean ^ _inverse;
-        cb.onToggle ~= (bool checked) { setting.boolean = checked ^ _inverse; };
-        return [cb];
+        CheckBox cb = render!CheckBox;
+        cb.checked = item.booleanDef(defaultValue);
+        cb.onToggle = (v) {
+            item.boolean = v;
+            setState(v, !v);
+        };
+        wrap(cb);
     }
 }
-
+/+
 /// ComboBox based setting with string keys
 class StringComboBoxItem : SettingsItem
 {
     private StringListValue[] _items;
 
-    this(string id, dstring label, StringListValue[] items)
-    {
-        super(id, label);
-        _items = items;
-    }
-
     override Widget[] createWidgets(Setting settings)
     {
-        auto lbl = new Label(_label);
-        lbl.id = _id ~ "-label";
         auto cb = new ComboBox(_items);
-        cb.id = _id;
-        cb.style.minWidth = 60;
         Setting setting = settings.settingByPath(_id);
         string itemID = setting.str = setting.str;
         int index = -1;
@@ -114,7 +88,7 @@ class StringComboBoxItem : SettingsItem
             if (itemIndex >= 0 && itemIndex < _items.length)
                 setting.str = _items[itemIndex].stringID;
         };
-        return [lbl, cb];
+        return cb;
     }
 }
 
@@ -123,19 +97,9 @@ class IntComboBoxItem : SettingsItem
 {
     private StringListValue[] _items;
 
-    this(string id, dstring label, StringListValue[] items)
-    {
-        super(id, label);
-        _items = items;
-    }
-
     override Widget[] createWidgets(Setting settings)
     {
-        auto lbl = new Label(_label);
-        lbl.id = _id ~ "-label";
         auto cb = new ComboBox(_items);
-        cb.id = _id;
-        cb.style.minWidth = 60;
         auto setting = settings.settingByPath(_id);
         long itemID = setting.integer = setting.integer;
         int index = -1;
@@ -153,7 +117,6 @@ class IntComboBoxItem : SettingsItem
             if (itemIndex >= 0 && itemIndex < _items.length)
                 setting.integer = _items[itemIndex].intID;
         };
-        return [lbl, cb];
     }
 }
 
@@ -161,22 +124,11 @@ class IntComboBoxItem : SettingsItem
 class FloatComboBoxItem : SettingsItem
 {
     private StringListValue[] _items;
-    private long _divider;
-
-    this(string id, dstring label, StringListValue[] items, long divider = 1000)
-    {
-        super(id, label);
-        _items = items;
-        _divider = divider;
-    }
+    private long _divider = 1000;
 
     override Widget[] createWidgets(Setting settings)
     {
-        auto lbl = new Label(_label);
-        lbl.id = _id ~ "-label";
         auto cb = new ComboBox(_items);
-        cb.id = _id;
-        cb.style.minWidth = 60;
         auto setting = settings.settingByPath(_id);
         setting.floating = setting.floating;
         long itemID = cast(long)(setting.floating * _divider + 0.5f);
@@ -199,132 +151,100 @@ class FloatComboBoxItem : SettingsItem
             if (itemIndex >= 0 && itemIndex < _items.length)
                 setting.floating = _items[itemIndex].intID / cast(double)_divider;
         };
-        return [lbl, cb];
     }
 }
-
-class NumberEditItem : SettingsItem
++/
+class IntegerEditItem : SettingsItem
 {
-    private int _minValue;
-    private int _maxValue;
-    private int _defaultValue;
+    long defaultValue;
+    long minValue = long.min;
+    long maxValue = long.max;
 
-    this(string id, dstring label, int minValue = int.min, int maxValue = int.max, int defaultValue = 0)
+    static protected class State : WidgetState
     {
-        super(id, label);
-        _minValue = minValue;
-        _maxValue = maxValue;
-        _defaultValue = defaultValue;
+        bool valid = true;
     }
 
-    override Widget[] createWidgets(Setting settings)
+    override protected State createState()
     {
-        auto lbl = new Label(_label);
-        lbl.id = _id ~ "-label";
-        auto ed = new EditLine(_label);
-        ed.id = _id ~ "-edit";
-        ed.style.minWidth = 60;
-        auto setting = settings.settingByPath(_id);
-        int n = cast(int)setting.integerDef(_defaultValue);
-        n = clamp(n, _minValue, _maxValue);
-        setting.integer = cast(long)n;
-        ed.text = to!dstring(n);
-        ed.onChange ~= (dstring str) {
-            const long v = parseLong(toUTF8(str), long.max);
-            if (v != long.max)
+        return new State;
+    }
+
+    override protected void build()
+    {
+        assert(settings);
+        Setting item = settings.settingByPath(path);
+        State st = use!State;
+
+        EditLine ed = render!EditLine;
+        if (st.valid)
+            ed.text = to!dstring(item.integerDef(defaultValue));
+        else
+            ed.attributes["error"];
+
+        ed.onChange = (str) {
+            try
             {
-                if (_minValue <= v && v <= _maxValue)
-                {
-                    setting.integer = v;
-                    ed.style.textColor = Color(0x000000);
-                }
-                else
-                {
-                    ed.style.textColor = Color(0xFF0000);
-                }
+                item.integer = clamp(to!long(str), minValue, maxValue);
+                setState(st.valid, true);
+                setState(str, null);
+            }
+            catch (Exception e)
+            {
+                setState(st.valid, false);
             }
         };
-        return [lbl, ed];
+        wrap(ed);
     }
 }
 
 class StringEditItem : SettingsItem
 {
-    private string _defaultValue;
+    string defaultValue;
 
-    this(string id, dstring label, string defaultValue)
+    override protected void build()
     {
-        super(id, label);
-        _defaultValue = defaultValue;
-    }
+        assert(settings);
+        Setting item = settings.settingByPath(path);
 
-    override Widget[] createWidgets(Setting settings)
-    {
-        auto lbl = new Label(_label);
-        lbl.id = _id ~ "-label";
-        auto ed = new EditLine;
-        ed.id = _id ~ "-edit";
-        ed.style.minWidth = 60;
-        auto setting = settings.settingByPath(_id);
-        string value = setting.str = setting.strDef(_defaultValue);
-        ed.text = toUTF32(value);
-        ed.onChange ~= (dstring str) { setting.str = toUTF8(str); };
-        return [lbl, ed];
+        EditLine ed = render!EditLine;
+        ed.text = toUTF32(item.strDef(defaultValue));
+        ed.onChange = (str) {
+            item.str = toUTF8(str);
+            setState(str, null);
+        };
+        wrap(ed);
     }
 }
-
+/+
+/// Field to edit a file name
 class FileNameEditItem : SettingsItem
 {
     private string _defaultValue;
 
-    this(string id, dstring label, string defaultValue)
-    {
-        super(id, label);
-        _defaultValue = defaultValue;
-    }
-
     override Widget[] createWidgets(Setting settings)
     {
-        import beamui.dialogs.filedialog;
-
-        auto lbl = new Label(_label);
-        lbl.id = _id ~ "-label";
         auto ed = new FileNameEditLine;
-        ed.id = _id ~ "-filename-edit";
-        ed.style.minWidth = 60;
         auto setting = settings.settingByPath(_id);
         string value = setting.str = setting.strDef(_defaultValue);
         ed.text = toUTF32(value);
         ed.onChange ~= (dstring str) { setting.str = toUTF8(str); };
-        return [lbl, ed];
     }
 }
 
+/// Field to edit an executable file name
 class ExecutableFileNameEditItem : SettingsItem
 {
     private string _defaultValue;
 
-    this(string id, dstring label, string defaultValue)
-    {
-        super(id, label);
-        _defaultValue = defaultValue;
-    }
-
     override Widget[] createWidgets(Setting settings)
     {
-        import beamui.dialogs.filedialog;
-
-        auto lbl = new Label(_label);
-        lbl.id = _id ~ "-label";
         auto ed = new FileNameEditLine;
-        ed.id = _id ~ "-filename-edit";
         ed.addFilter(FileFilterEntry(tr("Executable files"), "*.exe", true));
-        ed.style.minWidth = 60;
         auto setting = settings.settingByPath(_id);
         string value = setting.str = setting.strDef(_defaultValue);
         ed.text = toUTF32(value);
         ed.onChange ~= (dstring str) { setting.str = toUTF8(str); };
-        return [lbl, ed];
     }
 }
 
@@ -332,42 +252,20 @@ class PathNameEditItem : SettingsItem
 {
     private string _defaultValue;
 
-    this(string id, dstring label, string defaultValue)
-    {
-        super(id, label);
-        _defaultValue = defaultValue;
-    }
-
     override Widget[] createWidgets(Setting settings)
     {
-        import beamui.dialogs.filedialog;
-
-        auto lbl = new Label(_label);
-        lbl.id = _id ~ "-label";
         auto ed = new DirEditLine;
-        ed.id = _id ~ "-path-edit";
         ed.addFilter(FileFilterEntry(tr("All files"), "*.*"));
-        ed.style.minWidth = 60;
         auto setting = settings.settingByPath(_id);
         string value = setting.str = setting.strDef(_defaultValue);
         ed.text = toUTF32(value);
         ed.onChange ~= (dstring str) { setting.str = toUTF8(str); };
-        return [lbl, ed];
     }
 }
 
 /// Settings page - item of settings tree, can edit several settings
 class SettingsPage
 {
-    @property string id() const { return _id; }
-
-    @property dstring label() const { return _label; }
-
-    @property int childCount() const
-    {
-        return cast(int)_children.length;
-    }
-
     private
     {
         SettingsPage _parent;
@@ -375,12 +273,6 @@ class SettingsPage
         SettingsItem[] _items;
         string _id;
         dstring _label;
-    }
-
-    this(string id, dstring label)
-    {
-        _id = id;
-        _label = label;
     }
 
     /// Returns child page by index
@@ -401,124 +293,6 @@ class SettingsPage
         return addChild(new SettingsPage(id, label));
     }
 
-    @property int itemCount()
-    {
-        return cast(int)_items.length;
-    }
-
-    /// Returns page item by index
-    SettingsItem item(int index)
-    {
-        return _items[index];
-    }
-
-    SettingsItem addItem(SettingsItem item)
-    {
-        _items ~= item;
-        item._page = this;
-        return item;
-    }
-
-    /// Add checkbox (boolean value) for setting
-    CheckboxItem addCheckbox(string id, dstring label, bool inverse = false)
-    {
-        auto res = new CheckboxItem(id, label, inverse);
-        addItem(res);
-        return res;
-    }
-
-    /// Add EditLine to edit number
-    NumberEditItem addNumberEdit(string id, dstring label, int minValue = int.min,
-            int maxValue = int.max, int defaultValue = 0)
-    {
-        auto res = new NumberEditItem(id, label, minValue, maxValue, defaultValue);
-        addItem(res);
-        return res;
-    }
-
-    /// Add EditLine to edit string
-    StringEditItem addStringEdit(string id, dstring label, string defaultValue = "")
-    {
-        auto res = new StringEditItem(id, label, defaultValue);
-        addItem(res);
-        return res;
-    }
-
-    /// Add EditLine to edit filename
-    FileNameEditItem addFileNameEdit(string id, dstring label, string defaultValue = "")
-    {
-        auto res = new FileNameEditItem(id, label, defaultValue);
-        addItem(res);
-        return res;
-    }
-
-    /// Add EditLine to edit filename
-    PathNameEditItem addDirNameEdit(string id, dstring label, string defaultValue = "")
-    {
-        auto res = new PathNameEditItem(id, label, defaultValue);
-        addItem(res);
-        return res;
-    }
-
-    /// Add EditLine to edit executable file name
-    ExecutableFileNameEditItem addExecutableFileNameEdit(string id, dstring label, string defaultValue = "")
-    {
-        auto res = new ExecutableFileNameEditItem(id, label, defaultValue);
-        addItem(res);
-        return res;
-    }
-
-    StringComboBoxItem addStringComboBox(string id, dstring label, StringListValue[] items)
-    {
-        auto res = new StringComboBoxItem(id, label, items);
-        addItem(res);
-        return res;
-    }
-
-    IntComboBoxItem addIntComboBox(string id, dstring label, StringListValue[] items)
-    {
-        auto res = new IntComboBoxItem(id, label, items);
-        addItem(res);
-        return res;
-    }
-
-    FloatComboBoxItem addFloatComboBox(string id, dstring label, StringListValue[] items, long divider = 1000)
-    {
-        auto res = new FloatComboBoxItem(id, label, items, divider);
-        addItem(res);
-        return res;
-    }
-
-    /// Create page widget (default implementation creates empty page)
-    Widget createWidget(Setting settings)
-    {
-        auto panel = new Panel(_id, "settings-table");
-        if (itemCount == 0)
-            return panel;
-
-        auto title = new Label(_label);
-        title.isolateThisStyle();
-        title.setAttribute("title");
-        panel.addChild(title);
-
-        foreach (i; 0 .. itemCount)
-        {
-            Widget[] wts = item(i).createWidgets(settings);
-            if (wts.length == 1)
-            {
-                panel.addChild(wts[0]);
-                wts[0].setAttribute("full-row");
-            }
-            else
-            {
-                assert(wts.length == 2);
-                panel.addChild(wts[0]);
-                panel.addChild(wts[1]);
-            }
-        }
-        return panel;
-    }
-
     /// Returns true if this page is root page
     @property bool isRoot() const
     {
@@ -530,7 +304,51 @@ class SettingsPage
         return new TreeItem(_id, _label);
     }
 }
++/
+class SettingsPage : Panel
+{
+    dstring title;
+    Setting settings;
+    SettingsItem[] items;
 
+    override protected void build()
+    {
+        wrap(
+            render((Label t) {
+                t.text = title;
+            }),
+            render((SettingsList list) {
+                list.settings = settings;
+                list.items = items;
+            }),
+        );
+    }
+}
+
+class SettingsList : Panel
+{
+    Setting settings;
+    SettingsItem[] items;
+
+    override protected void build()
+    {
+        wrap(cast(uint)items.length * 2, delegate Widget(i) {
+            if (i % 2 == 0)
+            {
+                Label label = render!Label;
+                label.text = items[i / 2].title;
+                return label;
+            }
+            else
+            {
+                auto item = items[i / 2];
+                item.settings = settings;
+                return item;
+            }
+        });
+    }
+}
+/+
 class SettingsDialog : Dialog
 {
     private
@@ -561,10 +379,6 @@ class SettingsDialog : Dialog
         content.add(treeFrame, new Resizer, _frame);
         treeFrame.addChild(_tree);
         createControls(_pageTree, _tree.items);
-
-        content.isolateThisStyle();
-        treeFrame.isolateThisStyle();
-        _frame.isolateThisStyle();
 
         _tree.onSelect ~= &handleTreeItemSelection;
         if (_pageTree.childCount > 0)
